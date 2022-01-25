ae88f5d1be29aee0dfb3a6b59cd47c2417e760e6
uglyonlytoday <superjang3357@naver.com>
2022-01-25 23:02:37 +0900

    circle random color drawer

--- src/3/sketch.js
+++ src/3/sketch.js
@@ -1,1 +1,18 @@
 ///<reference path="../../intellisence/p5.global-mode.d.ts" />
+
+const screenWidth = document.documentElement.clientWidth;
+const screenHeight = document.documentElement.clientHeight;
+
+function setup() {
+	createCanvas(screenWidth, screenHeight);
+}
+
+x = 0
+
+function draw() {
+	ellipse(mouseX, mouseY, 100)
+}
+
+function mousePressed() {
+	background(random(255), random(255), random(255))
+}
